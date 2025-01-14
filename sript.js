17e3fde5efbf336f47addbf1b8d418435fb6e948
ali <yassine03@gmail.com>
2025-01-14 23:30:51 +0100

    14/1/2025

---
+++ sript.js
@@ -0,0 +1,9 @@
+let menu = document.getElementById("menuone");
+open = document.getElementById("open");
+close = document.getElementById("close");
+open.onclick = function () {
+  menu.style.display = "block";
+};
+close.onclick = function () {
+  menu.style.display = "none";
+};
